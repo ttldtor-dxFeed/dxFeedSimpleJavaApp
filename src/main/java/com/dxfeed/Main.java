33018b78b55132ae2d508412c1d34af44845b6bd
ttldtor <ttldtor@gmail.com>
2022-09-12 18:28:23 +0300

    N executors

--- src/main/java/com/dxfeed/Main.java
+++ src/main/java/com/dxfeed/Main.java
@@ -1,16 +1,37 @@
 package com.dxfeed;
 
-import com.dxfeed.model.QDService;
+import com.dxfeed.api.DXEndpoint;
+import com.dxfeed.api.DXFeedSubscription;
+import com.dxfeed.event.market.Quote;
 import com.dxfeed.tools.Speedometer;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.stream.LongStream;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         System.setProperty("scheme", "com.dxfeed.api.impl.DXFeedScheme");
 
-        QDService qdService = new QDService(new Speedometer(5000));
+        DXEndpoint endpoint = DXEndpoint.create();
 
-        qdService.testTnsSubscription("192.168.0.149:8888", Arrays.asList("AAPL", "IBM", "ETH/USD:GDAX"), 0);
+        endpoint.connect("demo.dxfeed.com:7300");
+
+        Speedometer speedometer = new Speedometer(5000);
+        List<DXFeedSubscription<Quote>> subs = new ArrayList<>();
+
+        LongStream.range(0, 10000).forEach(i -> {
+            DXFeedSubscription<Quote> sub = endpoint.getFeed().createSubscription(Quote.class);
+            sub.setExecutor(new ScheduledThreadPoolExecutor(1));
+            sub.addEventListener(list -> speedometer.addEvents(list.size()));
+            sub.addSymbols("AAPL");
+            subs.add(sub);
+        });
+
+        Thread.sleep(10001000);
+        subs.forEach(DXFeedSubscription::close);
+        endpoint.close();
     }
 }
